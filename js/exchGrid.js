49b5f32d23b07df7d5c98fba8934abf21f1a51b9
sg0703 <samgates@Sams-MBP.attlocal.net>
2021-07-26 18:10:31 -0400

    Converted to async/await, cleaned up code, added styling

--- js/exchGrid.js
+++ js/exchGrid.js
@@ -1,48 +1,32 @@
 /**
  * FX symbols by exchange 
  * Populates form with available exchanges
- * Once an exchange is chosen, populates chard with avail FX symbols
+ * Once an exchange is chosen, populates grid with avail FX symbols
  */
 
 // FinnHub API key
 const key = 'c162mdv48v6ootkka5hg';
 
+// grid configuration
 const exchColumnDefs = [
-    { field: "symbol",  filter: true},
-    { field: "displaySymbol", filter: true },
-    { field: "description", filter: true }
+    { field: "symbol",  filter: true, sortable: true },
+    { field: "displaySymbol", filter: true, sortable: true },
+    { field: "description", filter: true, sortable: true }
 ];
 
-// let the grid know which columns and what data to use
 const exchGridOptions = {
     columnDefs: exchColumnDefs,
-    //rowData: rowData,
     rowSelection: 'multiple'
 };
 
-// setup the grid after the page has finished loading
-document.addEventListener('DOMContentLoaded', () => {
-    const exchGridDiv = document.querySelector('#exchGrid');
-    new agGrid.Grid(exchGridDiv, exchGridOptions);
-});
+// load grid 
+const exchGridDiv = document.querySelector('#exchGrid');
+new agGrid.Grid(exchGridDiv, exchGridOptions);
 
-// pull available exchanges from FinnHub, populate drop-down list
+// load dropdown options 
 populateFormOptions();
 
-//
-const selectFxForm = document.getElementById('selectFxForm');
-
-selectFxForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    // get selected exchange from form
-    let exchSelect = document.getElementById("exchSelect");
-    let exchange = exchSelect.options[exchSelect.selectedIndex].text;
-
-    populateGrid(exchange);
-
-})
-
+// function to get available FX exchanges 
 async function populateFormOptions() {
     const response = await fetch(`https://finnhub.io/api/v1/forex/exchange?token=${key}`);
 
@@ -58,34 +42,28 @@ async function populateFormOptions() {
 
         formRoot.append(formSelect);
     })
-
-    console.log(exchanges)
 }
 
+// once user picks an exchange, query FinnHub for avail symbols
 async function populateGrid(selExchange) {
-    console.log(selExchange)
-
-    agGrid.simpleHttpRequest({
+    const data = await agGrid.simpleHttpRequest({
         url: `https://finnhub.io/api/v1/forex/symbol?exchange=${selExchange}&token=${key}`
     })
-    .then((data) => {
-        console.log(data)
-        /** 
-        let processedData = Object.keys(data.symbol).map((key) => { 
-        return { curr: key, quote: data.quote[key] }
-        });
-        **/
-    
-        exchGridOptions.api.setRowData(data);
-    })
-}
- 
-/**
- * 
-function getSelectedRows() {
-    var selectedNodes = gridOptions.api.getSelectedNodes();
-    var selectedData = selectedNodes.map(node => node.data);
 
-    console.log(selectedData);
+    // no processing necessary - data is ready to go to grid
+    exchGridOptions.api.setRowData(data);
 }
- */
+
+// set up event listener to handle form submission
+const selectFxForm = document.getElementById('selectFxForm');
+
+selectFxForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get selected exchange from form
+    let exchSelect = document.getElementById("exchSelect");
+    let exchange = exchSelect.options[exchSelect.selectedIndex].text;
+
+    populateGrid(exchange);
+
+})
--- js/fxGrid.js
+++ js/fxGrid.js
@@ -1,35 +1,32 @@
+/**
+ * Foreign Exchange rates US $ base currency
+ */
+
+// configure grid
 const fxColumnDefs = [
-{ field: "currency",  filter: true},
-{ field: "quote", filter: true },
+    { field: "currency",  filter: true, sortable: true },
+    { field: "quote", filter: true, sortable: true },
 ];
 
-// let the grid know which columns and what data to use
 const fxGridOptions = {
     columnDefs: fxColumnDefs,
-    //rowData: rowData,
     rowSelection: 'multiple'
 };
 
-// setup the grid after the page has finished loading
-document.addEventListener('DOMContentLoaded', () => {
-    const fxGridDiv = document.querySelector('#fxGrid');
-    new agGrid.Grid(fxGridDiv, fxGridOptions);
-});
+// build grid
+const fxGridDiv = document.querySelector('#fxGrid');
+new agGrid.Grid(fxGridDiv, fxGridOptions);
+getFxRates();
 
-function getSelectedRows() {
-    var selectedNodes = fxGridOptions.api.getSelectedNodes();
-    var selectedData = selectedNodes.map(node => node.data);
+// function to fet FX Rate data from FinnHub
+async function getFxRates() {
+    const data = await agGrid.simpleHttpRequest({
+        url: 'https://finnhub.io/api/v1/forex/rates?base=USD&token=c162mdv48v6ootkka5hg'
+    });
 
-    console.log(selectedData);
-}
-
-agGrid.simpleHttpRequest({
-    url: 'https://finnhub.io/api/v1/forex/rates?base=USD&token=c162mdv48v6ootkka5hg'
-})
-.then((data) => {
     let processedData = Object.keys(data.quote).map((key) => { 
-    return { currency: key, quote: data.quote[key] }
+        return { currency: key, quote: data.quote[key] }
     });
 
     fxGridOptions.api.setRowData(processedData);
-})
+}
