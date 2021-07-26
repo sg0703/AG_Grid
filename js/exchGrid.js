ae940ac54d0542a6b44bdc2ef859eee2c73fa17e
sg0703 <samgates@Sams-MBP.attlocal.net>
2021-07-26 17:17:06 -0400

    API now populating dropdown menu

--- js/exchGrid.js
+++ js/exchGrid.js
@@ -1,34 +1,85 @@
-const fxColumnDefs = [
-    { field: "currency",  filter: true},
-    { field: "quote", filter: true },
+/**
+ * FX symbols by exchange 
+ * Populates form with available exchanges
+ * Once an exchange is chosen, populates chard with avail FX symbols
+ */
+
+// FinnHub API key
+const key = 'c162mdv48v6ootkka5hg';
+
+const exchColumnDefs = [
+    { field: "symbol",  filter: true},
+    { field: "displaySymbol", filter: true },
+    { field: "description", filter: true }
 ];
 
 // let the grid know which columns and what data to use
-const fxGridOptions = {
-    columnDefs: columnDefs,
+const exchGridOptions = {
+    columnDefs: exchColumnDefs,
     //rowData: rowData,
     rowSelection: 'multiple'
 };
 
 // setup the grid after the page has finished loading
 document.addEventListener('DOMContentLoaded', () => {
-    const fxGridDiv = document.querySelector('#fxGrid');
-    new agGrid.Grid(fxGridDiv, fxGridOptions);
+    const exchGridDiv = document.querySelector('#exchGrid');
+    new agGrid.Grid(exchGridDiv, exchGridOptions);
 });
 
-/** 
-agGrid.simpleHttpRequest({
-    url: 'https://finnhub.io/api/v1/forex/rates?base=USD&token=c162mdv48v6ootkka5hg'
-})
-.then((data) => {
-    let processedData = Object.keys(data.quote).map((key) => { 
-    return { currency: key, quote: data.quote[key] }
-    });
+// pull available exchanges from FinnHub, populate drop-down list
+populateFormOptions();
 
-    fxGridOptions.api.setRowData(processedData);
-})
-*/
+//
+const selectFxForm = document.getElementById('selectFxForm');
 
+selectFxForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get selected exchange from form
+    let exchSelect = document.getElementById("exchSelect");
+    let exchange = exchSelect.options[exchSelect.selectedIndex].text;
+
+    console.log(exchange)
+
+})
+
+async function populateFormOptions() {
+    const response = await fetch(`https://finnhub.io/api/v1/forex/exchange?token=${key}`);
+
+    const exchanges = await response.json();
+
+    exchanges.forEach(exchange => {
+        console.log('here')
+        let formRoot = document.getElementById('exchSelect');
+
+        let formSelect = document.createElement('option');
+        formSelect.setAttribute('id',`exchange-${exchange}`);
+        formSelect.innerText = exchange;
+
+        formRoot.append(formSelect);
+    })
+
+    console.log(exchanges)
+}
+
+async function populateGrid(selExchange) {
+    console.log(selExch)
+
+    agGrid.simpleHttpRequest({
+        url: `https://finnhub.io/api/v1/forex/symbol?exchange=${selExchange}&token=${key}`
+    })
+    .then((data) => {
+        console.log(data)
+        /** 
+        let processedData = Object.keys(data.symbol).map((key) => { 
+        return { curr: key, quote: data.quote[key] }
+        });
+        **/
+    
+        fxGridOptions.api.setRowData(processedData);
+    })
+}
+ 
 /**
  * 
 function getSelectedRows() {
--- js/fxGrid.js
+++ js/fxGrid.js
@@ -1,23 +1,23 @@
-const columnDefs = [
+const fxColumnDefs = [
 { field: "currency",  filter: true},
 { field: "quote", filter: true },
 ];
 
 // let the grid know which columns and what data to use
-const gridOptions = {
-    columnDefs: columnDefs,
+const fxGridOptions = {
+    columnDefs: fxColumnDefs,
     //rowData: rowData,
     rowSelection: 'multiple'
 };
 
 // setup the grid after the page has finished loading
 document.addEventListener('DOMContentLoaded', () => {
-    const gridDiv = document.querySelector('#exchGrid');
-    new agGrid.Grid(gridDiv, gridOptions);
+    const fxGridDiv = document.querySelector('#fxGrid');
+    new agGrid.Grid(fxGridDiv, fxGridOptions);
 });
 
 function getSelectedRows() {
-    var selectedNodes = gridOptions.api.getSelectedNodes();
+    var selectedNodes = fxGridOptions.api.getSelectedNodes();
     var selectedData = selectedNodes.map(node => node.data);
 
     console.log(selectedData);
